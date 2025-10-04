439ec76c638887339b70a63ca8dc49ffe5348500
Himanshux07 <annikumar997@gmail.com>
2025-10-04 20:03:07 +0530

    Space optimization in FrogJump

--- Quetsions/src/FrogJump.java
+++ Quetsions/src/FrogJump.java
@@ -36,11 +36,31 @@ public class FrogJump {
         return dp[n];
     }
 
+    //Space Optimization
+    static int spaceOpt(int n, int[] arr) {
+        int prev=0;        // dp[i-1]
+        int prev2=0;       // dp[i-2]
+
+        for (int i=1;i<=n;i++) {
+            int left=prev+Math.abs(arr[i]-arr[i-1]);
+            int right=Integer.MAX_VALUE;
+            if (i>1)right=prev2+Math.abs(arr[i]-arr[i-2]);
+
+            int curr=Math.min(left,right);
+
+            // shift
+            prev2 = prev;
+            prev = curr;
+        }
+
+        return prev;
+    }
+
 
     public static void main(String[] args) {
-        System.out.println(rec(3,new int[]{30,10,60,10,60,50}));
-        System.out.println(memo(3,new int[]{30,10,60,10,60,50},new int[]{-1,-1,-1,-1}));
-        System.out.println(tabu(3,new int[]{30,10,60,10,60,50}));
+        System.out.println(rec(5,new int[]{30,10,60,10,60,50}));
+        System.out.println(memo(5,new int[]{30,10,60,10,60,50},new int[]{-1,-1,-1,-1,-1,-1}));
+        System.out.println(tabu(5,new int[]{30,10,60,10,60,50}));
     }
 
 }
