888b0c0315885628e6fb222bf3d897f1283a4231
Himanshux07 <annikumar997@gmail.com>
2025-10-07 22:52:01 +0530

    2D-DP recursive

---
+++ Quetsions/src/NinjaTraining.java
@@ -0,0 +1,28 @@
+public class NinjaTraining {
+    static int recursive(int [][] arr,int idx,int days){
+        if(days==0){
+            int maxi=0;
+            for(int i=0;i<3;i++){
+                if(i!=idx){
+                    maxi=Math.max(maxi,arr[days][i]);
+                }
+            }
+            return maxi;
+        }
+        int maxi=0;
+        for(int i=0;i<3;i++){
+            if(i!=idx){
+                int points=arr[days][i]+recursive(arr,i,days-1);
+                maxi=Math.max(maxi,points);
+            }
+        }
+        return maxi;
+    }
+    static int ninja(int [][] arr){
+        return recursive(arr,arr.length-1,3);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ninja(new int[][]{{2,1,3},{3,4,6},{10,1,6},{8,3,7}}));
+    }
+}
