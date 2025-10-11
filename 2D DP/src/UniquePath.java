a5627682f2d20649a7260a181587fde7ca3c5a6a
Himanshux07 <annikumar997@gmail.com>
2025-10-11 15:11:04 +0530

    2D-DP Unique Path memoization

--- 2D DP/src/UniquePath.java
+++ 2D DP/src/UniquePath.java
@@ -12,5 +12,16 @@ public class UniquePath {
         return left+right;
     }
 
+    //memoization
+    static int memo(int i,int j,int [][]dp){
+        if(i==0 && j==0){
+            return 1;
+        }
+        if(i<0 || j<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int left=recur(i,j-1);
+        int right=recur(i-1,j);
 
+        return dp[i][j]=left+right;
+    }
 }
