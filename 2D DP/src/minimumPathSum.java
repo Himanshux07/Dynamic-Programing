9045c0b62132c06ff4850a1e98b918a497dcce62
Himanshux07 <annikumar997@gmail.com>
2025-10-13 19:48:12 +0530

    minimum path recursive and memoization

--- 2D DP/src/minimumPathSum.java
+++ 2D DP/src/minimumPathSum.java
@@ -1,4 +1,16 @@
 public class minimumPathSum {
+
+    //recursion
+    public static int recur(int[][] grid,int i,int j){
+        if(i==0 && j==0) return grid[0][0];
+        if(i<0 || j<0) return (int)1e9;
+
+        int left=grid[i][j]+recur(grid,i,j-1);
+        int right=grid[i][j]+recur(grid,i-1,j);
+
+        return Math.min(left,right);
+    }
+
     // memoization
     public static int memo(int [][] grid,int i,int j,int [][]dp){
         if(i==0 && j==0) return grid[0][0];
