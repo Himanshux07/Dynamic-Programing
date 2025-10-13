39713aef688103c2b536a1be4a90388f632dc6ab
Himanshux07 <annikumar997@gmail.com>
2025-10-13 20:01:32 +0530

    minimum path tabulation

--- 2D DP/src/minimumPathSum.java
+++ 2D DP/src/minimumPathSum.java
@@ -21,4 +21,25 @@ public class minimumPathSum {
 
         return dp[i][j]=Math.min(left,right);
     }
+
+    //Tabulation
+    public static int tabu(int [][]grid){
+        int n=grid.length;
+        int m=grid[0].length;
+        int [][] dp= new int[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 && j==0) {
+                    dp[i][j]=grid[i][j];
+                    continue;
+                }
+                int left=(int)1e9;
+                int right=(int)1e9;
+                if(j>0) left=dp[i][j-1];
+                if(i>0) right=dp[i-1][j];
+                dp[i][j]=grid[i][j]+Math.min(left,right);
+            }
+        }
+        return dp[n-1][m-1];
+    }
 }
