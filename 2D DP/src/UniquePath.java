b435978229d38a1b678f6d3832b66b8efb2c929d
Himanshux07 <annikumar997@gmail.com>
2025-10-13 18:07:13 +0530

    uniquepath Space Optimization

--- 2D DP/src/UniquePath.java
+++ 2D DP/src/UniquePath.java
@@ -39,4 +39,20 @@ public class UniquePath {
         }
         return dp[m-1][n-1];
     }
+
+    //Space Optimization
+    public int val(int m,int n){
+        int [] prev=new int[n];
+        for(int i=0;i<m;i++){
+            int [] temp=new int[n];
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) temp[j]=1;
+                else{
+                    temp[j]=prev[j]+temp[j-1];
+                }
+            }
+            prev=temp;
+        }
+        return prev[n-1];
+    }
 }
