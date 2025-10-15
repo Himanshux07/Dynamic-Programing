3c729dba7293f6e98fbe09bc2f2a979d01b32e3e
Himanshux07 <annikumar997@gmail.com>
2025-10-15 22:01:20 +0530

    Triangle path memoization

--- 2D DP/src/TrianglePath.java
+++ 2D DP/src/TrianglePath.java
@@ -10,5 +10,14 @@ public class TrianglePath {
         return Math.min(down, diagonal);
     }
 
+    //memoization
+    static int memo(int [][] arr,int i,int j,int[][]dp){
+        if(i==arr.length-1) return arr[i][j];
+        if(dp[i][j]!=-1) return dp[i][j];
 
+        int down=arr[i][j]+memo(arr,i+1,j,dp);
+        int diagonal=arr[i][j]+memo(arr,i+1,j+1,dp);
+
+        return dp[i][j]=Math.min(down,diagonal);
+    }
 }
