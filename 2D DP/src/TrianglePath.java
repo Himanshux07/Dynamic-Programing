54b563b68f720e8264c6bf0a1b0eabd0e0d0cbf5
Himanshux07 <annikumar997@gmail.com>
2025-10-16 21:30:57 +0530

    Triangle path SpaceOpt

--- 2D DP/src/TrianglePath.java
+++ 2D DP/src/TrianglePath.java
@@ -41,4 +41,25 @@ public class TrianglePath {
         }
         return dp[0][0];
     }
+
+    //Space Optimization
+    static int spcOpt(List<List<Integer>> triangle){
+        int n=triangle.size();
+        int[] front=new int[n];
+        int[] curr=new int[n];
+
+        for(int j=0;j<n;j++){
+            front[j]=triangle.get(n-1).get(j);
+        }
+
+        for(int i=n-2;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                int down=triangle.get(i).get(j)+front[j];
+                int diag=triangle.get(i).get(j)+front[j+1];
+                curr[j]=Math.min(down,diag);
+            }
+            front=curr;
+        }
+        return front[0];
+    }
 }
