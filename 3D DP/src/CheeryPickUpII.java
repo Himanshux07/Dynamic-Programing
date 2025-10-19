0981822a21707ebbd8dc78bc3da2c9058313a1af
Himanshux07 <annikumar997@gmail.com>
2025-10-19 16:23:05 +0530

    CheeryPickUpII memoization

--- 3D DP/src/CheeryPickUpII.java
+++ 3D DP/src/CheeryPickUpII.java
@@ -30,5 +30,23 @@ public class CheeryPickUpII {
     }
 
     //memoization
-
+    int helper(int [][] arr,int i,int j1,int j2,int[][][] dp){
+        if(j1<0 || j1>=m || j2<0 || j2>=m){
+            return (int)-1e9;
+        }
+        if(i==n-1){
+            if(j1!=j2) return arr[i][j1]+arr[i][j2];
+            return arr[i][j1];
+        }
+        if(dp[i][j1][j2]!=-1) return dp[i][j1][j2];
+        int maxi=(int)-1e9;
+        for(int c1=-1;c1<=1;c1++){
+            for(int c2=-1;c2<=1;c2++){
+                maxi=Math.max(helper(arr,i+1,j1+c1,j2+c2,dp),maxi);
+            }
+        }
+        if(j1==j2) maxi+=arr[i][j1];
+        else maxi+=arr[i][j1]+arr[i][j2];
+        return dp[i][j1][j2]=maxi;
+    }
 }
