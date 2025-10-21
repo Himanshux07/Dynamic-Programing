3e376e685dd82465aadf093d15297acb4fd5f84e
Himanshux07 <annikumar997@gmail.com>
2025-10-21 16:14:20 +0530

    TargetSum memoization

--- Sequence/src/TargetSum.java
+++ Sequence/src/TargetSum.java
@@ -13,8 +13,20 @@ public class TargetSum {
         return notTake || Take;
     }
 
+    //memoization
+    static boolean memo(int [] arr,int target,int index,boolean[][] dp){
+        if(target==0) return true;
+        if(index==0) return arr[0]==target;
+        if(dp[index][target]) return true;
+        boolean notTake=rec(arr,target,index-1);
+        boolean Take=false;
+        if(arr[index]<=target)   Take=rec(arr,target-arr[index],index-1);
+
+        return dp[index][target]=notTake || Take;
+    }
 
     public static void main(String[] args) {
         System.out.println(rec(new int[]{2,4,11,3,9,16},7,2));
+        System.out.println();
     }
 }
