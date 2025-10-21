284da492bb8dcce4b5166fa514a54b4f76e2f384
Himanshux07 <annikumar997@gmail.com>
2025-10-21 15:54:32 +0530

    TargetSum recursive

--- Sequence/src/TargetSum.java
+++ Sequence/src/TargetSum.java
@@ -1,2 +1,20 @@
+// subsequence sum equals to target
 public class TargetSum {
+
+    // recursion
+    static boolean rec(int [] arr,int target,int index){
+        if(target==0) return true;
+        if(index==0) return arr[0]==target;
+
+        boolean notTake=rec(arr,target,index-1);
+        boolean Take=false;
+        if(arr[index]<=target)   Take=rec(arr,target-arr[index],index-1);
+
+        return notTake || Take;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(rec(new int[]{2,4,11,3,9,16},7,2));
+    }
 }
