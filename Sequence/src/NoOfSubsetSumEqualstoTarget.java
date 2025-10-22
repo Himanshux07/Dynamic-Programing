864a799abfd7338eeca2a2b1010ef13d36a96065
Himanshux07 <annikumar997@gmail.com>
2025-10-22 22:13:57 +0530

    tabulation

--- Sequence/src/NoOfSubsetSumEqualstoTarget.java
+++ Sequence/src/NoOfSubsetSumEqualstoTarget.java
@@ -4,10 +4,10 @@ public class NoOfSubsetSumEqualstoTarget {
     static int rec(int [] arr,int target,int index){
         if(target==0) return 1;
         if(index<0 || target<0) return 0;
-        int pick=rec(arr,target,index-1);
-        int notPickup=rec(arr,target-arr[index],index-1);
+        int notPick=rec(arr,target,index-1);
+        int pick=rec(arr,target-arr[index],index-1);
 
-        return pick+notPickup;
+        return pick+notPick;
     }
 
     // memoization
@@ -15,10 +15,29 @@ public class NoOfSubsetSumEqualstoTarget {
         if(target==0) return 1;
         if(index<0 || target<0) return 0;
         if(dp[index][target]!=0) return dp[index][target];
-        int pick=rec(arr,target,index-1);
-        int notPickup=rec(arr,target-arr[index],index-1);
+        int notPick=rec(arr,target,index-1);
+        int pick=rec(arr,target-arr[index],index-1);
 
-        return dp[index][target]=pick+notPickup;
+        return dp[index][target]=pick+notPick;
+    }
+     //tabulation
+    static int tabu(int [] arr,int target){
+        int n=arr.length;
+        int [][] dp= new int[n][target+1];
+        for(int i=0;i<n;i++){
+            dp[i][0]=1;
+        }
+        if(target>=arr[0]) dp[0][arr[0]]=1;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=target;j++){
+                int notPick=dp[i-1][j];
+                int pick=0;
+                if(j>=arr[i]) pick=dp[i-1][j-arr[i]];
+
+                dp[i][j]=pick+notPick;
+            }
+        }
+        return dp[n-1][target];
     }
 
     public static void main(String[] args) {
@@ -26,5 +45,6 @@ public class NoOfSubsetSumEqualstoTarget {
         int [][] dp = new int[6][6];
         System.out.println(rec(arr,5,5));
         System.out.println(memo(arr,5,5,dp));
+        System.out.println(tabu(arr,5));
     }
 }
