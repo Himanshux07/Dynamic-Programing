fc08a16fc47b78dc94792febf42941f877692f13
Himanshux07 <annikumar997@gmail.com>
2025-10-24 05:44:23 +0530

    tabulation

--- Sequence/src/coinExchange.java
+++ Sequence/src/coinExchange.java
@@ -13,4 +13,17 @@ public class coinExchange{
         return Math.min(pick,notPick);
     }
 
+    //tabulation
+    int tabu(int [] arr,int target,int index,int [][] dp){
+        if(index==0){
+            if(target%arr[0]==0) return target/arr[0];
+            return (int) 1e9;
+        }
+        if(dp[index][target]!=0) return dp[index][target];
+        int notPick=tabu(arr,target,index-1,dp);
+        int pick=(int) 1e9;
+        if(target>=arr[index]) pick=1+tabu(arr,target-arr[index],index,dp);
+
+        return dp[index][target]=Math.min(pick,notPick);
+    }
 }
