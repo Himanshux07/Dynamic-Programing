c181b43439cc6594c3a904b16908b2fa650b7f9f
Himanshux07 <annikumar997@gmail.com>
2025-10-24 16:39:03 +0530

    Space Optimization

--- Sequence/src/coinExchange.java
+++ Sequence/src/coinExchange.java
@@ -47,4 +47,26 @@ public class coinExchange{
         }
         return dp[n-1][target];
     }
+
+    //space optimization
+    static int spcOpt(int [] arr,int target){
+        int n=arr.length;
+        int [] prev= new int[target+1];
+
+        for(int i=0;i<=target;i++){
+            if(i%arr[0]==0) prev[i]=i/arr[0];
+            else prev[i]=(int)1e9;
+        }
+        for(int i=1;i<n;i++){
+            int [] curr= new int[target+1];
+            for(int j=0;j<=target;j++){
+                int notPick=prev[j];
+                int pick=(int) 1e9;
+                if(j>=arr[i]) pick=1+curr[target-arr[i]];
+
+                curr[j]=Math.min(pick,notPick);
+            }
+        }
+        return prev[target];
+    }
 }
