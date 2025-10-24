616e6bf32e547167566554eaedd8cce23c3c8696
Himanshux07 <annikumar997@gmail.com>
2025-10-24 05:42:27 +0530

    Recursive

--- Sequence/src/coinExchange.java
+++ Sequence/src/coinExchange.java
@@ -1,4 +1,15 @@
-public class coinExchange{
 
+public class coinExchange{
     //recursive
+    int rec(int [] arr,int target,int index){
+        if(index==0) {
+            if (target % arr[0] == 0) return target / arr[0];
+            return (int) 1e9;
+        }
+        int notPick=rec(arr,target,index-1);
+        int pick=(int) 1e9;
+        if(target>=arr[index]) pick=1+rec(arr,target-arr[index],index);
+
+        return Math.min(pick,notPick);
+    }
 }
