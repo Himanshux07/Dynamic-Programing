bbbc8e8dfc56f536c28a478a7c8ce63020ab1659
Himanshux07 <annikumar997@gmail.com>
2025-11-08 02:40:34 +0530

    LCS recursive

--- DP on Strings/src/LongestCommonSubsequence.java
+++ DP on Strings/src/LongestCommonSubsequence.java
@@ -1,12 +1,15 @@
 public class LongestCommonSubsequence {
 
     //recursive
-    static int rec(String s,int idx1,int idx2){
+    static int rec(String s1,String s2,int idx1,int idx2){
         if(idx1<0 || idx2<0) return 0;
-        if(s.charAt(idx1)==s.charAt(idx2)){
-            return 1+rec(s,idx1-1,idx2-1);
+        if(s1.charAt(idx1)==s2.charAt(idx2)){
+            return 1+rec(s1,s2,idx1-1,idx2-1);
         }
-        else return Math.max(rec(s,idx1-1,idx2),rec(s,idx1,idx2-1));
+        else return Math.max(rec(s1,s2,idx1-1,idx2),rec(s1,s2,idx1,idx2-1));
     }
 
+    public static void main(String[] args) {
+        System.out.println(rec("himanshu","palak",7,4));
+    }
 }
