6abde1e8de8adc7a2684eec7fed3a99d45be6592
Himanshux07 <annikumar997@gmail.com>
2025-11-08 03:24:04 +0530

    LCS tabulation

--- DP on Strings/src/LongestCommonSubsequence.java
+++ DP on Strings/src/LongestCommonSubsequence.java
@@ -19,5 +19,24 @@ public class LongestCommonSubsequence {
         else return dp[idx1][idx2]=Math.max(memo(s1,s2,idx1-1,idx2,dp),memo(s1,s2,idx1,idx2-1,dp));
     }
 
+    //tabulation
+    static int tabu(String s1,String s2){
+        int n=s1.length();
+        int m=s2.length();
+        //shifting of index 0--->-1,n----->n-1
+        int [][] dp= new int[n+1][m+1];
+        for(int i=0;i<=n;i++) dp[i][0]=0;
+        for(int i=0;i<=m;i++) dp[0][i]=0;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                     dp[i][j]=1+dp[i-1][j-1];
+                }
+                else dp[i][j]=Math.max(dp[i][j-1],dp[i-1][j]);
+            }
+        }
+        return dp[n][m];
+    }
 
 }
