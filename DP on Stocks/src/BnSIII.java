d063b4c15ebe7b632fd1e20e94240454e0d59447
Himanshux07 <annikumar997@gmail.com>
2025-11-18 14:56:58 +0530

    stock 3 Space Opt

--- DP on Stocks/src/BnSIII.java
+++ DP on Stocks/src/BnSIII.java
@@ -52,4 +52,29 @@ public class BnSIII {
         }
         return dp[0][0];
     }
+
+    // space opt
+    static  int spcOpt(int [] arr){
+        int n= arr.length;
+        int [] ahead = new int[5];
+        ahead[4]=0;
+        for(int i=0;i<5;i++){
+            ahead[i]=0;
+        }
+        for(int idx=n-1;idx>=0;idx--){
+            int [] crr = new int[5];
+            for(int tran=0;tran<4;tran++){
+                int profit=0;
+                if(tran%2==0){
+                    profit=Math.max(-(arr[idx])+ahead[tran+1],ahead[tran]);
+                }
+                else{
+                    profit=Math.max((arr[idx])+ahead[tran+1],ahead[tran]);
+                }
+                crr[tran]=profit;
+            }
+            ahead=crr;
+        }
+        return ahead[0];
+    }
 }
