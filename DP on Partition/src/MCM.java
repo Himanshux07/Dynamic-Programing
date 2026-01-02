69eb0fc9667ffee40c2a8191213eaf2aca0b0622
Himanshux07 <annikumar997@gmail.com>
2026-01-02 18:53:36 +0530

    MCM memo

--- DP on Partition/src/MCM.java
+++ DP on Partition/src/MCM.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class MCM {
     //recursion way
     static int rec(int i,int j,int[] arr){
@@ -10,7 +12,23 @@ public class MCM {
         return ans;
     }
 
+    //memoization
+    static int memo(int i,int j,int[] arr,int[][] dp){
+        if(i==j) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int ans=(int)1e9;
+        for(int k=i;k<=j-1;k++){
+            int steps=arr[i-1]*arr[k]*arr[j]+memo(i,k,arr,dp)+memo(k+1,j,arr,dp);
+            ans=Math.min(ans,steps);
+        }
+        return dp[i][j]=ans;
+    }
     public static void main(String[] args) {
         System.out.println(rec(1,4,new int[]{10,20,30,40,50}));
+        int[][] dp = new int[5][5];
+        for(int i=0;i<5;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        System.out.println(memo(1,4,new int[]{10,20,30,40,50},dp));
     }
 }
